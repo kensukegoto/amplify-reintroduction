d6a2868c487b6d83b7d80758b21029544f36d389
kensuke goto <kensuke.goto.1981@gmail.com>
2021-03-13 21:51:56 +0900

    add auth + graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import Amplify from "aws-amplify";
+import {AmplifyAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
+import {AuthState, onAuthUIStateChange} from '@aws-amplify/ui-components';
+import awsmobile from './aws-exports';
+import Todos from './Todos';
 
-function App() {
-  return (
+Amplify.configure(awsmobile);
+
+const App = () => {
+  const [authState, setAuthState] = React.useState<AuthState>();
+
+  useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todos/>
+      <AmplifySignOut/>
     </div>
+  ) : (
+    <AmplifyAuthenticator/>
   );
 }
 
